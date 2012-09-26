c3d5abee12f31fb6b6a29999e061dafaba58b865
Phil Marsden <phil.marsden@softwire.com>
2012-09-26 13:22:27 +0000

    kids

--- LegoMindstorms/KidsProjects/Bob.py
+++ LegoMindstorms/KidsProjects/Bob.py
@@ -8,17 +8,16 @@ from Sonar import Sonar
 from nxt.motor import PORT_A, PORT_B, PORT_C
 from nxt.sensor import PORT_4, PORT_3, PORT_2, PORT_1
 
-print "Robot4"
-robot = Robot('ICTCLUB4')
+print "Robot5"
+robot = Robot('ICTCLUB5')
 robot.extend(Light(PORT_3))
-robot.extend(Mover(PORT_C, PORT_B))
 
 i = 0
-while i < 5 :
+while i < 60 :
   robot.lightred()
   sleep(1)
   robot.lightblue()
   sleep(1)
   i=i+1
-robot.selfdestruct(99,99)
+robot.lightoff()
 robot.__del__()
