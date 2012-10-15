9d4798870fecfa114882da3ac70ed142293a7965
Phil Marsden <phil.marsden@softwire.com>
2012-10-15 07:47:25 +0000

    Freddit and Will

--- LegoMindstorms/KidsProjects/Freddie-Walrus.py
+++ LegoMindstorms/KidsProjects/Freddie-Walrus.py
@@ -1,3 +1,4 @@
+
 import sys
 from time import sleep
 from Robot import Robot
@@ -12,11 +13,12 @@ print "Robot4"
 robot = Robot('ICTCLUB4')
 robot.extend(Light(PORT_3))
 robot.extend(Mover(PORT_B, PORT_C))
+robot.extend(Sonar(PORT_1))
 
-a=1
-while a == 1:
- robot.forward(1,50)
- if robot.howfar() < 10:
-  robot.turnleft(1,50)
-
+#Program starts here
+for x in range(10,10000):
+	robot.forward(0.1,80)
+	if robot.howfar() < 40 :
+		robot.turnright(4,80)
+        
 robot.__del__()
